d6c5bc0f515b099e2d3a368561e87321b68fd865
Kishore Saha <sahakishore4@gmail.com>
2022-11-13 08:11:41 +0530

    Add project files.

---
+++ ManagementSystemRBCodeFirst/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystemRBCodeFirst
+{
+    //ManagementSystem Entities
+
+    //Roles Entity
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    //Employee Entity
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+
+
+        //Navigation property
+
+    }
+
+    //EmployeeDetails Entity
+    public class EmployeeDetails
+    {
+        public int EmpDetailId { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+        //Navigation property
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
